ed98cc274cf5c056cad28dc080eee14faddeb461
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2023-02-11 22:38:49 +0530

    MovieHero section completed

--- src/pages/Movie.Page.jsx
+++ src/pages/Movie.Page.jsx
@@ -6,6 +6,7 @@ import { MovieContext } from '../context/Movie.context';
 import Slider from 'react-slick';
 import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
 import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import MovieHero from '../components/MovieHero/MovieHero.Component';
 
 const MoviePage = () => {
     const { id } = useParams();
@@ -53,11 +54,42 @@ const MoviePage = () => {
 
     const settingCast = {};
 
-    const settings = {};
+    const settings = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 3,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    initialSlide: 4,
+                },
+            },
+        ],
+    };
 
     return (
         <>
-            {/* <MovieHero /> */}
+            < MovieHero />
             <div className="my-12 container px-4 lg-ml-20 lg:w-2/3">
                 <div className="flex flex-col items-start gap-3">
                     <h1 className="text-gray-800 font-bold text-2xl">
